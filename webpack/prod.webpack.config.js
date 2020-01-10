8a761e476fd0601aee9dd13ea5aa0a695f95a961
resolritter <joao.paulo.silvasouza@hotmail.com>
2020-01-10 04:21:03 -0300

    predictable hashed chunks based on the vendored module's name

--- webpack/prod.webpack.config.js
+++ webpack/prod.webpack.config.js
@@ -1,27 +1,20 @@
 const path = require("path")
 const lodash = require("lodash")
+
+// for hashing the chunk names
+var crypto = require('crypto')
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const CopyWebpackPlugin = require("copy-webpack-plugin")
 
 const baseConfiguration = require("./base.webpack.config")
-const { mergeConfigurations } = require("./utils")
+const { mergeConfigurations, hashChunk } = require("./utils")
 
-module.exports = mergeConfigurations(baseConfiguration, {
+var configuration = mergeConfigurations(baseConfiguration, {
   mode: "production",
   output: {
     path: path.resolve(baseConfiguration.context, "./public"),
     filename: "[name]-[contenthash].js",
     chunkFilename: "[name]-[contenthash].js",
-  },
-  devtool: "source-map",
-  module: {
-    rules: [
-      {
-        enforce: "pre",
-        test: /\.js$/,
-        loader: "source-map-loader",
-      },
-    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -37,15 +30,32 @@ module.exports = mergeConfigurations(baseConfiguration, {
     splitChunks: {
       chunks: "all",
       maxInitialRequests: Infinity,
-      minSize: 30000,
+      minSize: 2048,
       cacheGroups: {
-        defaultVendors: {
+        fromNpm: {
           test: /[\\/]node_modules[\\/]/,
-          name: function({ rawRequest, _buildHash }) {
-            return `npm.${rawRequest}__${_buildHash}.js`
+          name: function({ rawRequest }) {
+            return `npm.${hashChunk(path.basename(rawRequest))}`
           },
         },
       },
     },
   },
 })
+
+if (process.env["SOURCEMAPS"]) {
+  configuration = mergeConfigurations(configuration, {
+    devtool: "source-map",
+    module: {
+      rules: [
+        {
+          enforce: "pre",
+          test: /\.js$/,
+          loader: "source-map-loader",
+        },
+      ],
+    },
+  })
+}
+
+module.exports = configuration
--- webpack/utils.js
+++ webpack/utils.js
@@ -1,4 +1,12 @@
 const _ = require("lodash")
+const crypto = require("crypto")
+
+const chunkSalt = process.env['CHUNKS_HASH_SALT']
+
+if (!chunkSalt) {
+  console.error(`Salt ${chunkSalt} for production build chunks is not set`)
+  exit(0)
+}
 
 module.exports = {
   mergeConfigurations(baseConfiguration, otherConfiguration) {
@@ -11,4 +19,8 @@ module.exports = {
       }
     })
   },
+  hashChunk(chunkName) {
+    const hashBuffer = crypto.pbkdf2Sync(chunkName, chunkSalt, 4, 16, 'md5')
+    return hashBuffer.toString('hex')
+  }
 }
