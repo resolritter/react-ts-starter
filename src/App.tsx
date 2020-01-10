8b415bcdd8490d49ce3958971860b34ec474a3dd
resolritter <joao.paulo.silvasouza@hotmail.com>
2020-01-10 04:29:00 -0300

    make assets work for production builds

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import * as React from "react"
 
+import reactLogo from "../assets/react.png"
+
 function App(): JSX.Element {
-  return <h1>Hello World!</h1>
+  return (
+    <>
+    <img src={reactLogo} alt="react logo" />
+    <h1>Hello World!</h1>
+    </>
+  )
 }
 
 export default App
